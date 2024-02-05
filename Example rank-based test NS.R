e3b4aeb6e89c3d716d606778f8c6e16b8eab697c
DuyNgoStats <49419194+DuyNgoStats@users.noreply.github.com>
2024-02-05 15:41:02 -0500

    update

--- Example rank-based test NS.R
+++ Example rank-based test NS.R
@@ -10,7 +10,7 @@ n_time = 200
 ## Number of trials
 trials = 100
 ## Numer of EEG channels
-n_channel = 255
+n_channel = 15
 ## Numer of frequency band.
 ## delta 1-3Hz, theta 4-8Hz, alpha 9-12Hz, beta 13-30Hz, gamma 30-50Hz.
 n_indpt = 5   
@@ -21,19 +21,38 @@ module = evol(rep(1.001, 5), 0.00001, trials, n_indpt)
 
 ## A multivariate time series (MTS) X(t) = [X_1(t), X_2(t), X_3(t)]
 ## Each cluster X_i(t) and Y_i(t) have nM elements.
-nM = 85
+nM = 5
 m0 = rep(0, nM) 
-nM2 = 85
+nM2 = 5
 m02 = rep(0, nM2) 
-## Mixing matrix for the first MTV X^{(1)}(t)
-M1 = rbind(cbind(rep(1, nM),  rep(0.2, nM),   m0,   m0,   m0),
-           cbind(rep(1, nM),  rep(0.5, nM),   m0,     m0,    m0),
-           cbind(rep(0.1, nM), rep(0.2, nM),  rep(0.5, nM),   m0,  m0))
-## Mixing matrix for the second MTV X^{(2)}(t)
-M2 = rbind(cbind(rep(1, nM),  rep(0.2, nM),   m0,   m0,   m0),
-           cbind(rep(0.2, nM),  m0,   m0,     rep(0.2, nM),    m0),
-           cbind(m02, m02,  rep(0.5, nM2),   rep(0.2, nM2),  m02))
+##### Mixing matrix M1 (Equal case)
+M11 = rbind(cbind(rep(1, nM),  rep(0.2, nM),   m0,   m0,   m0),
+            cbind(rep(1, nM),  rep(0.5, nM),   m0,     m0,    m0),
+            cbind(rep(0.1, nM2), rep(0.2, nM2),  rep(0.5, nM2),   m02,  m02))
+M12 = rbind(cbind(rep(1, nM),  rep(0.2, nM),   m0,   m0,   m0),
+            cbind(rep(1, nM),  rep(0.5, nM),   m0,     m0,    m0),
+            cbind(rep(0.1, nM2), rep(0.2, nM2),  rep(0.5, nM2),   m02,  m02))
+M1 = rbind(M11, M12)
 
+
+##### Mixing matrix M2 (Non-equal case)
+M21 = rbind(cbind(rep(1, nM),  rep(0.2, nM),   m0,   m0,   m0),
+            cbind(rep(1, nM),  rep(0.5, nM),   m0,     m0,    m0),
+            cbind(rep(0.1, nM2), rep(0.2, nM2),  rep(0.5, nM2),   m02,  m02))
+M22 = rbind(cbind(rep(1, nM),  rep(0.2, nM),   m0,   m0,   m0),
+            cbind(rep(0.2, nM),  m0,   m0,     rep(0.2, nM),    m0),
+            cbind(m02, m02,  rep(0.5, nM2),   rep(0.2, nM2),  m02))
+M2 = rbind(M21, M22)
+
+
+####### Abrupt change
+M31 = rbind(cbind(rep(0.5, nM),  rep(0.2, nM),   m0,   m0,   m0),
+            cbind(rep(0.5, nM),  rep(0.5, nM),   m0,     m0,    m0),
+            cbind(rep(0.2, nM2), rep(0.2, nM2),  rep(0.2, nM2),   m02,  m02))
+M32 = rbind(cbind(rep(0.5, nM),  rep(0.2, nM),   m0,   m0,   m0),
+            cbind(rep(0.5, nM),  rep(0.5, nM),   m0,     m0,    m0),
+            cbind(rep(0.2, nM2), rep(0.2, nM2),  rep(0.2, nM2),   m02,  m02))
+M3 = rbind(M31, M32)
 
 ## Design covariance matrix
 Sigma1 = matrix(0.55, n_channel, n_channel)
@@ -48,21 +67,40 @@ diag(Sigma) = 1
 ################################################################
 ################ 2 MTV X and Y have similar latent sources
 ################################################################
-M = rbind(M1, M1)
-X = generate_time(module, sigma=rep(.05, trials), Sigma, M, 
-                   2*n_channel, n_indpt, trials, n_time)
-X.ts = X$realization
+X.ts1 = generate_time2(module, sigma=rep(.05, trials), Sigma, M1, 2*n_channel, 
+                       n_indpt, trials, n_time)$realization
 
+X.ts2 = generate_time2(module, sigma=rep(.05, trials), Sigma, M3, 2*n_channel, 
+                       n_indpt, trials, n_time)$realization
+
+X.ts =  abind(X.ts1[,1:100,],X.ts2[,101:200,],along=2)
+
+######## Plot non-stationary X(t)
+X.sim = X.ts[c(1, 20), , 1]
+plot(X.sim[1,], type="l", xlab = "Time", ylab="", 
+     cex.lab=1.6, cex.axis=1.7, cex.main=1.7, cex.sub=1.7, 
+     lwd=2, ylim = c(min(X.sim)-0.5, max(X.sim)+0.5))
+lines(X.sim[2,], lwd=2, lty = 2)
+legend('top', legend=c( expression(paste('X'^(1),'(t, 1)')), 
+                        expression(paste('X'^(2),'(t, 1)'))), 
+       horiz=TRUE, inset=c(0, -0.17), xpd=TRUE,
+       lty=1:2, cex=1.55,title = '', text.font=2, box.lty=0)
+
+acf(X.sim[1,],cex.lab=1.6, cex.axis=1.7, 
+    main = '', 
+    cex.main=1.7, cex.sub=1.7,lwd=2)
+
+####### Perform tests on two samples of coherence matrices
 cohX = array(0,dim = c(n_channel, n_channel, trials))
 for(j in 1:trials){
-  X.sim = X.ts[1:255,,j]
+  X.sim = X.ts[1:15,,j]
   f.smooth = SpectrumEstimate(X.sim, n_time, span=30, n_channel)
   cohX[,,j] = CoherenceEstimate(f.smooth,c(1:3))
 }
 
 cohY = array(0,dim = c(n_channel, n_channel, trials))
 for(j in 1:trials){
-  X.sim = X.ts[256:510,,j]
+  X.sim = X.ts[16:30,,j]
   f.smooth = SpectrumEstimate(X.sim, n_time, span=30, n_channel)
   cohY[,,j] = CoherenceEstimate(f.smooth,c(1:3))
 }
@@ -80,21 +118,24 @@ round(result, 2)
 ################################################################
 ################ 2 MTV X and Y have different latent sources
 ################################################################
-M = rbind(M1, M2)
-X = generate_time(module, sigma=rep(.05, trials), Sigma, M, 
-                   2*n_channel, n_indpt, trials, n_time)
-X.ts = X$realization
+X.ts1 = generate_time2(module, sigma=rep(.05, trials), Sigma, M2, 2*n_channel, 
+                       n_indpt, trials, n_time)$realization
+
+X.ts2 = generate_time2(module, sigma=rep(.05, trials), Sigma, M3, 2*n_channel, 
+                       n_indpt, trials, n_time)$realization
+
+X.ts =  abind(X.ts1[,1:100,],X.ts2[,101:200,],along=2)
 
 cohX = array(0,dim = c(n_channel, n_channel, trials))
 for(j in 1:trials){
-  X.sim = X.ts[1:255,,j]
+  X.sim = X.ts[1:15,,j]
   f.smooth = SpectrumEstimate(X.sim, n_time, span=30, n_channel)
   cohX[,,j] = CoherenceEstimate(f.smooth,c(1:3))
 }
 
 cohY = array(0,dim = c(n_channel, n_channel, trials))
 for(j in 1:trials){
-  Y.sim = X.ts[256:510,,j]
+  Y.sim = X.ts[16:30,,j]
   f.smooth = SpectrumEstimate(Y.sim, n_time, span=30, n_channel)
   cohY[,,j] = CoherenceEstimate(f.smooth,c(1:3))
 }
